82d420a4f2080093c6628b74aa12172893a92957
Lines <lines.sultzer@gmail.com>
2025-11-15 11:35:55 -0500

    cleaned up re renders and added lazy loading

--- src/components/NeuralBackground.tsx
+++ src/components/NeuralBackground.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { memo } from "react";
 import NeuralMap from "../assets/NeuralMap.svg";
 
-export const NeuralBackground = () => (
+export const NeuralBackground: React.FC = memo(() => (
   <div className="absolute inset-0 overflow-hidden pointer-events-none">
     <img
       src={NeuralMap}
       alt=""
       className="w-full h-full object-cover opacity-[0.15]"
+      loading="lazy"
+      decoding="async"
     />
   </div>
-);
+));
+
+NeuralBackground.displayName = "NeuralBackground";
